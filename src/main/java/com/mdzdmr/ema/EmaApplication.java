b8f744aa7298e3880a34eadd5ac25eeb869fb6fc
Musti9143 <mustafaduezdemir@hotmail.com>
2026-08-06 15:16:06 +0200

    Initial commit

--- src/main/java/com/mdzdmr/ema/EmaApplication.java
+++ src/main/java/com/mdzdmr/ema/EmaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmaApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(EmaApplication.class, args);
+    }
 
 }
--- src/test/java/com/mdzdmr/ema/EmaApplicationTests.java
+++ src/test/java/com/mdzdmr/ema/EmaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EmaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
