8ec5720612f6a8f8c1d5fbadb3fbce3d4f52f2a2
Musti9143 <mustafaduezdemir@hotmail.com>
2026-08-11 17:52:13 +0200

    Repository impl.

---
+++ src/main/java/com/mdzdmr/ema/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.mdzdmr.ema.repository;
+
+import com.mdzdmr.ema.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+
+}
