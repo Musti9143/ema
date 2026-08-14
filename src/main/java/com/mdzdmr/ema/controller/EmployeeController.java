16531b3783d9d2725c514c73cfd590adb02bcac5
Musti9143 <mustafaduezdemir@hotmail.com>
2026-08-14 11:22:45 +0200

    service/controller alignment to new exception handling and dtos

--- src/main/java/com/mdzdmr/ema/controller/EmployeeController.java
+++ src/main/java/com/mdzdmr/ema/controller/EmployeeController.java
@@ -1,81 +1,70 @@
 package com.mdzdmr.ema.controller;
 
 import com.mdzdmr.ema.dto.CreateEmployeeRequest;
+import com.mdzdmr.ema.dto.EmployeeResponse;
 import com.mdzdmr.ema.dto.UpdateEmployeeRequest;
-import com.mdzdmr.ema.entity.Employee;
+import com.mdzdmr.ema.mapper.EmployeeMapper;
 import com.mdzdmr.ema.service.EmployeeService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
+    private final EmployeeMapper mapper;
 
-    public EmployeeController(EmployeeService employeeService){
+    public EmployeeController(EmployeeService employeeService,  EmployeeMapper employeeMapper) {
         this.employeeService = employeeService;
+        this.mapper = employeeMapper;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Employee> getEmployee(@PathVariable Long id){
-        Optional<Employee> optionalEmployee = employeeService.getEmployeeById(id);
+    public ResponseEntity<EmployeeResponse> getEmployee(@PathVariable Long id){
 
-        return optionalEmployee
-                .map(ResponseEntity::ok)
-                .orElseGet(()-> ResponseEntity.notFound().build());
+        EmployeeResponse employeeResponse = mapper.toResponse(employeeService.getEmployeeById(id));
+        return ResponseEntity.ok(employeeResponse);
     }
 
     @GetMapping
-    public ResponseEntity<List<Employee>> getEmployees(){
-        List<Employee> employeeList = employeeService.getAllEmployees();
-        return ResponseEntity.ok(employeeList);
+    public ResponseEntity<List<EmployeeResponse>> getEmployees(){
+        List<EmployeeResponse> employeeResponseList = employeeService.getAllEmployees()
+                .stream().map(mapper::toResponse).toList();
+        return ResponseEntity.ok(employeeResponseList);
     }
 
     @PostMapping
-    public ResponseEntity<Employee> createEmployee(@RequestBody @Valid CreateEmployeeRequest createEmployeeRequest){
-        Optional<Employee> optionalEmployee = employeeService.createEmployee(mapToEmployee(createEmployeeRequest));
-        if (optionalEmployee.isEmpty())
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        Employee employee = optionalEmployee.get();
-        URI location = URI.create("/api/v1/employees/" + employee.getId());
-        return ResponseEntity.created(location).body(employee);
+    public ResponseEntity<EmployeeResponse> createEmployee(@RequestBody @Valid CreateEmployeeRequest createEmployeeRequest){
+
+        EmployeeResponse employeeResponse = mapper.toResponse(employeeService.createEmployee(mapper.toEmployee(createEmployeeRequest)));
+        return ResponseEntity.created(URI.create("/api/v1/employees/" + employeeResponse.id())).body(employeeResponse);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @Valid @RequestBody UpdateEmployeeRequest updateEmployeeRequest){
-        Optional<Employee> optionalEmployee = employeeService.updateEmployee(id, mapToEmployee(updateEmployeeRequest));
-        return optionalEmployee
-                .map(ResponseEntity::ok)
-                .orElseGet(()-> ResponseEntity.notFound().build());
+    @PatchMapping("/{id}")
+    public ResponseEntity<EmployeeResponse> updateEmployee(@PathVariable Long id, @Valid @RequestBody UpdateEmployeeRequest updateEmployeeRequest){
+
+        EmployeeResponse employeeResponse = mapper.toResponse(
+                employeeService.updateEmployee(id, mapper.toEmployee(updateEmployeeRequest)));
+
+        return ResponseEntity.ok(employeeResponse);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteEmployee(@PathVariable Long id){
-        if (employeeService.deleteEmployee(id)){
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.notFound().build();
-    }
 
-    private Employee mapToEmployee(UpdateEmployeeRequest updateEmployeeRequest){
-        return new Employee(updateEmployeeRequest.firstName(), updateEmployeeRequest.lastName(), updateEmployeeRequest.email(), updateEmployeeRequest.jobTitle());
-    }
-
-    private Employee mapToEmployee(CreateEmployeeRequest createEmployeeRequest) {
-        return new Employee(createEmployeeRequest.firstName(), createEmployeeRequest.lastName(), createEmployeeRequest.email(), createEmployeeRequest.jobTitle());
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
     }
 }
